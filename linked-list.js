9bc187e212be6a245fb5257c807c8b6887120391
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-07-30 19:51:03 +0000

    Implement skeletal factory for list and its node

---
+++ linked-list.js
@@ -0,0 +1,50 @@
+function list() {
+   return {
+      append(value) {
+
+      },
+      
+      prepend(value) {
+
+      },
+
+      size() {
+
+      },
+
+      head() {
+
+      },
+
+      tail() {
+
+      },
+
+      at(index) {
+
+      },
+
+      pop() {
+
+      },
+
+      contains(value) {
+
+      },
+
+      find(value) {
+
+      },
+
+      toString() {
+
+      },
+   }
+}
+
+function node(value = null, next = null) {
+   return {
+      value,
+      next,
+   }
+}
