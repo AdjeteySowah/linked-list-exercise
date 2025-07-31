ea7986bd3d0c1b661c9402d5a45d4d4c0534f773
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-07-31 20:40:38 +0000

    Implement append and prepend fxns in createList

--- linked-list.js
+++ linked-list.js
@@ -1,11 +1,44 @@
-function list() {
+function createList() {
+   let headNode = null;
+   let tailNode = null;
+
    return {
       append(value) {
+         let newNode = {
+            prev: null,
+            value,
+            next: null,
+         };
 
+            // when list is empty
+         if (headNode === null) {
+            headNode = newNode;
+            tailNode = newNode;
+         }
+
+            // when there's at least one node in the list
+         if (headNode !== null) {
+            tailNode.next = newNode;
+            newNode.prev = tailNode;
+            tailNode = newNode;      
+         } 
       },
       
       prepend(value) {
+         let newNode = {
+            prev: null,
+            value,
+            next: this,
+         };
 
+         if (headNode === null) {
+            headNode = newNode;
+            tailNode = newNode;
+         }
+
+         if (headNode !== null) {
+            headNode = newNode;
+         }
       },
 
       size() {
@@ -41,6 +74,7 @@ function list() {
       },
    }
 }
+
 
 function node(value = null, next = null) {
    return {
