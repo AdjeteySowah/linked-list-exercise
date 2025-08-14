e127630042a0773dbad2df92cfef6ac98368171a
Emmanuel Adjetey Sowah <adjetey.sowah.321@gmail.com>
2025-08-14 16:17:16 +0000

    Import factory, populate list and test

---
+++ main.js
@@ -0,0 +1,12 @@
+import { createList } from "./linked-list";
+
+const list = createList();
+
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
+
+console.log(list.toString());
